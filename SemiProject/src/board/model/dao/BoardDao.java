bf6f59336c24e2b76a11a66f085be48a208a278d
user2 <user2@user2-PC>
2019-10-18 16:42:14 +0900

    게시판싹엎음.19.10.18 4:42

--- SemiProject/src/board/model/dao/BoardDao.java
+++ SemiProject/src/board/model/dao/BoardDao.java
@@ -1,5 +1,55 @@
 package board.model.dao;
 
+import static common.JDBCTemplate.close;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
 public class BoardDao {
+	
+	Properties prop = new Properties();
+
+	public BoardDao() {
+		String fileName = BoardDao.class.getResource("/sql/board/board-query.properties").getPath();
+		
+		try {
+			prop.load(new FileReader(fileName));
+		} catch (FileNotFoundException e) {		
+			e.printStackTrace();
+		} catch (IOException e) {	
+			e.printStackTrace();
+		}
+	}
+	
+	public int getListCount(Connection conn) {
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		int listCount = 0;
+		
+		String query = prop.getProperty("getListCount");
+		
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(query);
+			
+			if(rs.next()) {
+				listCount=rs.getInt(1);	
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(stmt);
+			close(rs);
+		}
+		return listCount;
+	}
 
 }
--- SemiProject/src/board/model/service/BoardService.java
+++ SemiProject/src/board/model/service/BoardService.java
@@ -1,5 +1,27 @@
 package board.model.service;
 
+import static common.JDBCTemplate.close;
+import static common.JDBCTemplate.commit;
+import static common.JDBCTemplate.getConnection;
+import static common.JDBCTemplate.rollback;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import board.model.dao.BoardDao;
+
 public class BoardService {
 
+	
+	
+	
+	public int getListCount() {
+		Connection conn = getConnection();
+		
+		int listCount = new BoardDao().getListCount(conn);
+		
+		close(conn);
+		
+		return listCount;
+	}
 }
