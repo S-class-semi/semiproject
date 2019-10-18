235e51b33e40d31b51704407b076f68e4f6c6364
JangJunHa <JangJunHa@LAPTOP-G5M95BAS>
2019-10-18 16:54:46 +0900

    성호 합치기 완료 (게시판)

--- SemiProject/src/board/controller/BoardListServlet.java
+++ SemiProject/src/board/controller/BoardListServlet.java
@@ -28,9 +28,6 @@ public class BoardListServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
